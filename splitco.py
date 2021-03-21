d4649541e39b2e54645a5fe1f6932e0308b04887
joshallends <joshallends.com>
2021-03-21 17:53:03 -0400

    Small tweaks to match README write up

--- splitco.py
+++ splitco.py
@@ -10,11 +10,13 @@ filename = sys.argv[1]
 with open('input/{}'.format(filename)) as f:
     data = json.loads(f.read())
 
-ts = pd.to_datetime(data['date']).strftime('%Y%m%d')
+description = data['description']
+timestamp = pd.to_datetime(data['timestamp']).strftime('%Y%m%d')
 
 items = pd.json_normalize(data, record_path =['items']).set_index('item')
 items['weights'] = (items.cost + items.adjustment)/items['contributors'].str.len()
 df = items[['weights']].join(items.contributors.str.join('|').str.get_dummies())
 df = df.apply(lambda x: multiply_columns(x, df['weights'])).drop('weights', axis=1)
 df.loc['total']= df.sum()
-df.to_csv('output/cost-shares-{}.csv'.format(ts))
+print(df)
+df.to_csv('output/{}-{}.csv'.format(description,timestamp))
