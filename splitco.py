ed42cc2fc6e6f70ce2d23709bb6398abcf4d36ca
joshallends <joshallends.com>
2021-04-19 20:55:44 -0400

    class for splitco

--- splitco.py
+++ splitco.py
@@ -1,22 +1,49 @@
+import os
 import sys
 import json
 import pandas as pd
 
-# Function to multiply binary indicators by weights
-def multiply_columns(column, weights):
-    return column * weights
 
-filename = sys.argv[1]
-with open('input/{}'.format(filename)) as f:
-    data = json.loads(f.read())
+class SplitCo():
+    def __init__(self):
+        super().__init__()
 
-description = data['description']
-timestamp = pd.to_datetime(data['timestamp']).strftime('%Y%m%d')
+        self.basepath = os.getcwd()
+        self.input_dir = os.path.join(self.basepath,'input')
+        self.output_dir = os.path.join(self.basepath,'output')
+        self.input_file = 'receipt-template.json'
+        self.description = self.get_description()
+        self.time_stamp = self.get_timestamp()
 
-items = pd.json_normalize(data, record_path =['items']).set_index('item')
-items['weights'] = (items.cost + items.adjustment)/items['contributors'].str.len()
-df = items[['weights']].join(items.contributors.str.join('|').str.get_dummies())
-df = df.apply(lambda x: multiply_columns(x, df['weights'])).drop('weights', axis=1)
-df.loc['total']= df.sum()
-print(df)
-df.to_csv('output/{}-{}.csv'.format(description,timestamp))
+    def file_open(self):
+        with open(os.path.join(self.input_dir, self.input_file)) as f:
+            return json.loads(f.read())
+
+    def get_description(self):
+        json_data = self.file_open()
+        return json_data['description']
+
+    def get_timestamp(self):
+        json_data = self.file_open()
+        return pd.to_datetime(json_data['timestamp']).strftime('%Y%m%d')
+
+    def multiply_columns(self, column, weights):
+        return column * weights
+
+    def json_to_df(self):
+        return pd.json_normalize(self.file_open(), record_path =['items']).set_index('item')
+
+    def split_costs(self):
+        items = self.json_to_df()
+        items['weights'] = (items.cost + items.adjustment)/items['contributors'].str.len()
+        df = items[['weights']].join(items.contributors.str.join('|').str.get_dummies())
+        df = df.apply(lambda x: self.multiply_columns(x, df['weights'])).drop('weights', axis=1)
+        df.loc['total']= df.sum()
+        return df
+
+
+if __name__ == "__main__":
+    split = SplitCo()
+    split.input_file = sys.argv[1] 
+    print(split.split_costs())
+
